defd1125a8c4b2840d81e35e87bc3157061fed68
Vivek Sheth <vsheth@hhaexchange.com>
2021-02-15 17:23:35 +0530

    Day1 files

---
+++ Day1/Day1Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day1Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = SlowOperationAsync();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);  // 2 - Main
+            }
+            Console.WriteLine("Slow operation result : {0}", task.Result); // 4 - Main
+            Console.WriteLine("Main operation completed : {0}", Thread.CurrentThread.ManagedThreadId); // 5 - Main
+        }
+
+        static async Task<int> SlowOperationAsync()
+        {
+            Console.WriteLine("Slow operation started on : {0}", Thread.CurrentThread.ManagedThreadId); // 1 - Main
+            await Task.Delay(2000);
+            Console.WriteLine("Slow operation completed on : {0}", Thread.CurrentThread.ManagedThreadId); // 3 - Worker
+            return 42;
+        }
+    }
+}
---
+++ Day1/WebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AboutMe()
+        {
+            Person p = new Person();
+            p.Name = "Vsheth";
+            ViewData["Name"] = "Vivek";
+            ViewBag.location = "Ahmedabad";
+            return View(p);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
